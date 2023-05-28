2eb709979c4965459e0a0f95768005d42ad75023
M. Arief Budiman <ma48ud2@gmail.com>
2023-05-28 15:33:33 +0700

    test workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4 text-blue-800">Welcome to Project Hanan</h1>
-      <p className="text-lg text-gray-600 mb-8">This is a sample homepage for the Project Hanan website.</p>
+      <p className="text-lg text-gray-600 mb-8">This is a homepage for the Project Hanan website.</p>
       <div className="w-64 h-48 bg-white rounded-md shadow-md relative">
         <Image
           src="/images/homepage.jpg"
